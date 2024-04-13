6d7d842967317d57d97d375ad8d5cdd386c68e70
Marko Lojanica <marko.lojanica1991@gmail.com>
2024-04-13 12:09:50 +0200

    fixes for data grid

--- src/Screens/Home.screen.tsx
+++ src/Screens/Home.screen.tsx
@@ -1,20 +1,33 @@
 import useFetchItems from "../customHooks/useFetchItems"
 import { Box } from "@mui/material"
-import { DataGrid } from "@mui/x-data-grid"
+import { DataGrid, GridRowParams } from "@mui/x-data-grid"
 import { columns } from "../config/gridConfig.tsx"
 import Loader from "../components/Loader/Loader.component.tsx"
 import Errorbox from "../components/ErrorBox/Errorbox.component.tsx"
+import { useCallback } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Home = (): JSX.Element => {
 	const { items, loading, error } = useFetchItems()
-
+	const navigate = useNavigate()
+	const handleRowClick = useCallback(
+		(params: GridRowParams) => {
+			navigate(`details/${params.id}`)
+		},
+		[navigate]
+	)
 	return loading ? (
 		<Loader />
 	) : error ? (
 		<Errorbox />
 	) : (
 		<Box sx={{ height: "calc(100vh - 200px)" }}>
-			<DataGrid rows={items} columns={columns} />
+			<DataGrid
+				rows={items}
+				columns={columns}
+				onRowClick={handleRowClick}
+				sx={{ cursor: "pointer" }}
+			/>
 		</Box>
 	)
 }
